efc09a392c1a68eb3a744513c57ca3cf164be4de
happy30 <jordivdh@hotmail.com>
2016-10-11 16:26:17 +0200

    started hookscript

---
+++ HanzakiProject/Assets/Scripts/Player/GrapplingHook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrapplingHook : MonoBehaviour {
+
+    public StatsManager stats;
+
+    void Awake()
+    {
+        stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+    }
+    void Update ()
+    {
+	    if(stats.grapplingHookUnlocked)
+        {
+            if(Input.GetKeyDown(InputManager.Hook))
+            {
+
+            }
+        }
+	}
+}
