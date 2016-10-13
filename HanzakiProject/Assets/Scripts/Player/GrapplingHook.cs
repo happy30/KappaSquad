4c6e6ab1e048e6b243e0b82071284f09eb4c7f69
happy30 <jordivdh@hotmail.com>
2016-10-13 13:59:00 +0200

    Fixed camera position for SS

--- HanzakiProject/Assets/Scripts/Player/GrapplingHook.cs
+++ HanzakiProject/Assets/Scripts/Player/GrapplingHook.cs
@@ -4,19 +4,52 @@ using System.Collections;
 public class GrapplingHook : MonoBehaviour {
 
     public StatsManager stats;
+    public bool canHook;
+    public Transform hook;
+    public float clawSpeed;
+
+    public GameObject claw;
+    GameObject spawnedClaw;
+    LineRenderer _line;
+    public Vector3[] linePositions;
+
+    public bool grabbing;
+
+    public Rigidbody _rb;
 
     void Awake()
     {
         stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+        _rb = GetComponent<Rigidbody>();
+        _line = GetComponent<LineRenderer>();
     }
     void Update ()
     {
 	    if(stats.grapplingHookUnlocked)
         {
-            if(Input.GetKeyDown(InputManager.Hook))
+            if(Input.GetKeyDown(InputManager.Hook) && canHook)
             {
-
+                spawnedClaw = (GameObject)Instantiate(claw, transform.position, Quaternion.identity);
+                spawnedClaw.transform.LookAt(hook);
+                grabbing = true;
             }
+        }
+
+        if(grabbing)
+        {
+            spawnedClaw.transform.position = Vector3.MoveTowards(spawnedClaw.transform.position, hook.transform.position, clawSpeed * Time.deltaTime);
+            linePositions[0] = transform.position;
+            linePositions[1] = spawnedClaw.transform.position;
+            _line.SetPositions(linePositions);
+
+            if(Vector3.Distance(spawnedClaw.transform.position, hook.transform.position) < 0.1f)
+            {
+                _rb.velocity = (hook.transform.position - transform.position)
+            }
+        }
+        else
+        {
+            
         }
 	}
 }
--- HanzakiProject/Assets/Scripts/Player/GrapplingHookScript.cs
+++ HanzakiProject/Assets/Scripts/Player/GrapplingHookScript.cs
@@ -3,21 +3,33 @@ using System.Collections;
 
 public class GrapplingHookScript : MonoBehaviour
 {
+    public GrapplingHook hook;
+    public StatsManager stats;
+
+    void Awake()
+    {
+        stats = GameObject.Find("GameManager").GetComponent<StatsManager>();
+        hook = GameObject.Find("Player").GetComponent<GrapplingHook>();
+    }
+
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+        if(col.gameObject.tag == "Player" && stats.grapplingHookUnlocked)
         {
             Debug.Log("HOOK");
             Camera.main.GetComponent<CameraController>().hookObject = gameObject;
+            hook.canHook = true;
+            hook.hook = transform;
 
         }
     }
 
     void OnTriggerExit(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+        if(col.gameObject.tag == "Player" && stats.grapplingHookUnlocked)
         {
             Camera.main.GetComponent<CameraController>().hookObject = null;
+            hook.canHook = false;
         }
     }
 
