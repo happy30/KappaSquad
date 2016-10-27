7e900cce053fb843df66b9b73218845e90548cf0
happy30 <jordivdh@hotmail.com>
2016-10-27 15:26:33 +0200

    added arne's stuff

---
+++ HanzakiProject/Assets/Scripts/LocationEnter.cs
@@ -0,0 +1,17 @@
+﻿//LocationEnter by Arne
+using UnityEngine;
+using System.Collections;
+
+public class LocationEnter : MonoBehaviour {
+
+	public string currentLVL;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HanzakiProject/Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,25 @@
+﻿//OptionsManager by Arne
+using UnityEngine;
+using System.Collections;
+
+public class OptionsManager : MonoBehaviour {
+	
+	public GameObject changeKeyPanel;
+	
+	// Use this for initialization
+	void Awake () 
+	{
+	
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	//KeyBindings press a key and get the option to change it
+	public void KeyBindings () //void moet char/string worden.
+	{
+		/*als een button wordt gepressed moet hij een text weergeven waarin
+		je kan cancelen en confirmen */ 
+	}
+}
