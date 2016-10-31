1858272650f83206391a5b1c7c623532e64c01fd
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2016-10-31 16:22:20 +0100

    LocationEnter.cs done needs to be checked

--- HanzakiProject/Assets/Scripts/Environment/LocationEnter.cs
+++ HanzakiProject/Assets/Scripts/Environment/LocationEnter.cs
@@ -1,14 +1,17 @@
 ﻿//LocationEnter by Arne
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LocationEnter : MonoBehaviour {
 
+	public GameObject levelName;
 	public string currentLVL;
+	
 	// Use this for initialization
-	void Start () {
-		
-		
+	void Awake () 
+	{
+		levelName.GetComponent<Text>().text = currentLVL;
 	}
 	// Update is called once per frame
 	void Update () {
