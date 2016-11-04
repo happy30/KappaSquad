d0831ec261c7e55821a9d02b05bd3c1e64d82d6e
happy30 <jordivdh@hotmail.com>
2016-11-04 15:03:38 +0100

    shurikens can destroy destructibles

--- HanzakiProject/Assets/Scripts/Environment/DestructibleScript.cs
+++ HanzakiProject/Assets/Scripts/Environment/DestructibleScript.cs
@@ -9,7 +9,7 @@ public class DestructibleScript : MonoBehaviour
     GameObject spawnedParticleObject;
 
 
-    public void Destroy ()
+    public void DestroyObject ()
     {
         Destroy(spawnedParticleObject = (GameObject)Instantiate(particleObject, transform.position, Quaternion.identity), 3);
         Destroy(gameObject);
