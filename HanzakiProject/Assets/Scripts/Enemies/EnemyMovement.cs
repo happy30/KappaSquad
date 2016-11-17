5b7fbc24acfda34d0a64c2338d9f4e9fcb2a4486
Alieke <aliekevanderlelie@hotmail.com>
2016-11-17 13:27:03 +0100

    bug Fix

--- HanzakiProject/Assets/Scripts/Enemies/EnemyMovement.cs
+++ HanzakiProject/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -84,7 +84,7 @@ public class EnemyMovement : MonoBehaviour
             }
         }
         playerDistance = Vector3.Distance(player.position, transform.position);
-        if (gameObject.GetComponent<EnemySight>().playerInView == true && playerDistance < outOfRange && enemyStates != States.Attacking)
+        if (gameObject.GetComponent<EnemySight>().playerInView == true && playerDistance < outOfRange && enemyStates != States.Attacking && !player.GetComponent<PlayerController>().invulnerable)
         {
             enemyStates = States.Chasing;
         }
